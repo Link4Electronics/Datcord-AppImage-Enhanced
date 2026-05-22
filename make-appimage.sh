cbf22758a372e07f0277b30fcefdee00354ce1f4
Link4Electronics <link4electronics@gmail.com>
2026-05-21 21:38:21 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,7 +10,7 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/256x256/apps/datcord.png
 export DESKTOP=/usr/share/applications/datcord.desktop
-export STARTUPWMCLASS=
+export STARTUPWMCLASS=datcord
 export DEPLOY_GTK=1
 export GTK_DIR=gtk-3.0
 
